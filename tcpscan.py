95075a8ca4724cc5406c676e29850982f4c3a561
ehis <divadefe@proton.me>
2024-01-15 06:38:56 +0100

    scan some IP address

---
+++ tcpscan.py
@@ -0,0 +1,19 @@
+import socket
+
+target_host = "arielahomecare.co.uk"
+target_port = 80
+
+# create a socket object client
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#connect to client
+client.connect((target_host,target_port))
+
+#sent to data
+client.send(b"GET / HTTP/1.1\r\nHost: arielahomecare.co.uk\r\n\r\n")
+
+#receive some date
+response = client.recv(4096)
+
+    print(response.decode())
+    client.close()
